b3120d93a441f48547e77e8938954ec2695ce907
Pallavikori64 <118381430+Pallavikori64@users.noreply.github.com>
2025-07-11 21:41:54 +0530

    Update Central.sol

--- Fake Product Identification Using Blockchain/Contract/Central.sol
+++ Fake Product Identification Using Blockchain/Contract/Central.sol
@@ -8,40 +8,37 @@ contract Central {
     address[] walletAddresses;
 
     mapping(address => address) walletAddressToSmartContractAddress;
+    mapping(address => string) public companyNames;
+    mapping(address => uint256) public companyCreationTime;
 
-    function createSmartContract() public {
+    
+    event CompanyCreated(address owner, address contractAddress, string companyName);
+    event ProductAdded(address companyAddress, address ownerAddress);
+
+    function createSmartContract(string memory _companyName) public {
+        require(walletAddressToSmartContractAddress[msg.sender] == address(0), "Already registered");
+
         Company companyContract = new Company(msg.sender);
         walletAddresses.push(address(companyContract));
-        walletAddressToSmartContractAddress[msg.sender] = address(
-            companyContract
-        );
+        walletAddressToSmartContractAddress[msg.sender] = address(companyContract);
+        companyNames[msg.sender] = _companyName;
+        companyCreationTime[msg.sender] = block.timestamp;
+
+        
+        emit CompanyCreated(msg.sender, address(companyContract), _companyName);
     }
 
-    function getCompanySmartContractAddress(address _walletAddress)
-        public
-        view
-        returns (address)
-    {
+    function getCompanySmartContractAddress(address _walletAddress) public view returns (address) {
         return walletAddressToSmartContractAddress[_walletAddress];
     }
 
-    function addproduct(
+    function addProduct(
         address _ownerAddress,
         address _contractAddress,
         uint256[] memory _products
     ) public returns (string memory) {
+        
+        emit ProductAdded(_contractAddress, _ownerAddress);
         return Company(_contractAddress).addProducts(_ownerAddress, _products);
-    }
-
-    function checkProduct(address _contractAddress, uint256 _productHashCode)
-        public
-        view
-        returns (string memory)
-    {
-        return Company(_contractAddress).verifyProduct(_productHashCode);
-    }
-
-    function retrieve(address _val) public pure returns (address) {
-        return _val;
     }
 }
